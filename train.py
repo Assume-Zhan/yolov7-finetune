d82eea0b6238eafda229b08752c344fd74db793f
assume <assume0701@gmail.com>
2023-09-12 19:42:00 +0000

    Modify train code

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from pathlib import Path
 
 import numpy as np
 import torch.nn as nn
@@ -10,40 +11,65 @@ import yaml
 from torch.cuda import amp
 from tqdm import tqdm
 
+import test
+
 from models.yolo import Model
 from utils.autoanchor import check_anchors
 from utils.datasets import create_dataloader
-from utils.general import labels_to_class_weights, init_seeds, check_img_size, set_logging, one_cycle
-from utils.loss import ComputeLossOTA
-from utils.torch_utils import ModelEMA, select_device, intersect_dicts
+from utils.general import labels_to_class_weights, init_seeds, one_cycle, increment_path, fitness
+from utils.loss import ComputeLossOTA, ComputeLoss
+from utils.torch_utils import ModelEMA, intersect_dicts
 
-BATCH_SIZE = 4
-EPOCHS = 50
+BATCH_SIZE = 1
+EPOCHS = 55
 
-def train(hyp, device):
+def train():
     
     batch_size = BATCH_SIZE
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Configure
     cuda = device.type != 'cpu'
     init_seeds(1)
+    
+    # Open Required Files : data, cfg, hyperparameters
     with open("data/Aquarium/data.yaml") as f:
         data_dict = yaml.load(f, Loader=yaml.SafeLoader)  # data dict
+    with open("data/hyp.scratch.p5.yaml") as f:
+        hyp = yaml.load(f, Loader=yaml.SafeLoader)  # load hyps
+        
+    # Prepare data
+    dataloader, dataset = create_dataloader(data_dict['train'], 640, batch_size, 32, hyp=hyp, augment=True)
 
-    nc = int(data_dict['nc'])  # number of classes
-    names = data_dict['names']  # class names
- 
     ckpt = torch.load('./weight/yolov7x_training.pt', map_location=device)
+    nc = int(data_dict['nc'])
     model = Model("cfg/training/yolov7.yaml", ch=3, nc=nc).to(device)
     state_dict = intersect_dicts(ckpt['model'].float().state_dict(), model.state_dict(), exclude=['anchor'])
     model.load_state_dict(state_dict, strict=False)
+    print("Transferred %g/%g items" % (len(state_dict), len(model.state_dict())))  # report
     
-    train_path = data_dict['train']
-    test_path = data_dict['val']
-
+    # Freeze
+    freeze = [f"model.{x}." for x in range(0)]  # parameter names to freeze (full or partial)
+    for k, v in model.named_parameters():
+        v.requires_grad = True  # train all layers
+        if any(x in k for x in freeze):
+            print("freezing %s" % k)
+            v.requires_grad = False
+    
+    nl = model.model[-1].nl
+    model.nc = nc  # attach number of classes to model
+    model.names = data_dict["names"]
+    model.hyp = hyp  # attach hyperparameters to model
+    model.gr = 1.0  # iou loss ratio (obj_loss = 1.0 or iou)
+    model.class_weights = labels_to_class_weights(dataset.labels, nc).to(device) * nc  # attach class weights
+    hyp['box'] *= 3. / nl  # scale to layers
+    hyp['cls'] *= nc / 80. * 3. / nl  # scale to classes and layers
+    hyp['obj'] *= (640 / 640) ** 2 * 3. / nl  # scale to image size and layers
+    hyp["label_smoothing"] = 0.0
+ 
     # Optimizer, Goal : Get a larger batch size by accumulating gradient
-    nbs = 64
-    accumulate = max(round(nbs / batch_size), 1)
+    accumulate = max(round(64 / batch_size), 1)
+    hyp["weight_decay"] *= BATCH_SIZE * accumulate / 64  # scale weight_decay
 
     pg0, pg1, pg2 = [], [], []  # optimizer parameter groups
     for k, v in model.named_modules():
@@ -115,7 +141,17 @@ def train(hyp, device):
     optimizer.add_param_group({'params': pg1, 'weight_decay': hyp['weight_decay']})  # add pg1 with weight_decay
     optimizer.add_param_group({'params': pg2})  # add pg2 (biases)
     del pg0, pg1, pg2
+    
+    # Directories
+    save_dir = Path(increment_path(Path("/yolov7/runs/train") / "exp", exist_ok=False))
+    wdir = save_dir / "weights"
+    wdir.mkdir(parents=True, exist_ok=True)  # make dir
+    weight_pt = wdir / "weight.pt"
+    results_file = save_dir / "results.txt"
 
+    # Init
+    best_fitness = 0.0
+    init_seeds(1)
     lf = one_cycle(1, hyp['lrf'], EPOCHS)  # cosine 1->hyp['lrf']
     scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
 
@@ -124,47 +160,64 @@ def train(hyp, device):
     if ema and ckpt.get('ema'):
         ema.ema.load_state_dict(ckpt['ema'].float().state_dict())
         ema.updates = ckpt['updates']
-
-    del ckpt, state_dict
-
-    # Image sizes
-    gs = max(int(model.stride.max()), 32)  # grid size (max stride)
-    nl = model.model[-1].nl  # number of detection layers (used for scaling hyp['obj'])
-    imgsz, imgsz_test = [check_img_size(x, gs) for x in [640, 640]]  # verify imgsz are gs-multiples
-    dataloader, dataset = create_dataloader(train_path, imgsz, batch_size, gs, hyp=hyp, augment=True)
+        
+    # Results
+    if ckpt.get("training_results") is not None:
+        results_file.write_text(ckpt["training_results"])  # write results.txt
+    
+    # Save run settings
+    with open(save_dir / "hyp.yaml", "w") as f:
+        yaml.dump(hyp, f, sort_keys=False)
+    
     nb = len(dataloader)  # number of batches
     
     # Anchors
-    check_anchors(dataset, model=model, thr=hyp['anchor_t'], imgsz=imgsz)
+    check_anchors(dataset, model=model, thr=hyp['anchor_t'], imgsz=640)
     model.half().float()  # pre-reduce anchor precision
 
     # Model parameters
-    hyp['box'] *= 3. / nl  # scale to layers
-    hyp['cls'] *= nc / 80. * 3. / nl  # scale to classes and layers
-    hyp['obj'] *= (imgsz / 640) ** 2 * 3. / nl  # scale to image size and layers
-    model.nc = nc  # attach number of classes to model
-    model.hyp = hyp  # attach hyperparameters to model
-    model.gr = 1.0  # iou loss ratio (obj_loss = 1.0 or iou)
-    model.class_weights = labels_to_class_weights(dataset.labels, nc).to(device) * nc  # attach class weights
-    model.names = names
-
+    nw = max(round(hyp["warmup_epochs"] * nb), 1000)  # number of warmup iterations, max(3 epochs, 1k iterations)
+    results = (0, 0, 0, 0, 0, 0, 0)  # P, R, mAP@.5, mAP@.5-.95, val_loss(box, obj, cls)
     scheduler.last_epoch = -1
     scaler = amp.GradScaler(enabled=cuda)
     compute_loss_ota = ComputeLossOTA(model)
+    compute_loss = ComputeLoss(model)  # init loss class
+    
+    del ckpt
+    
+    testloader, testset = create_dataloader(data_dict['val'], 640, batch_size * 2, 32, hyp=hyp, augment=True, pad = 0.5, rect = True)
     
     # Start training
     for epoch in range(EPOCHS):  # epoch ------------------------------------------------------------------
         model.train()
 
+        mloss = torch.zeros(4, device=device)  # mean losses
+        print(("\n" + "%10s" * 8) % ("Epoch", "gpu_mem", "box", "obj", "cls", "total", "labels", "img_size"))
+        pbar = tqdm(enumerate(dataloader), total=nb)  # progress bar
         optimizer.zero_grad()
-        for i, (imgs, targets, paths, _) in tqdm(enumerate(dataloader), total=nb):  # batch -------------------------------------------------------------
+
+        optimizer.zero_grad()
+        for i, (imgs, targets, paths, _) in pbar:
             ni = i + nb * epoch  # number integrated batches (since train start)
             imgs = imgs.to(device, non_blocking=True).float() / 255.0  # uint8 to float32, 0-255 to 0.0-1.0
 
+            # Warmup
+            if ni <= nw:
+                xi = [0, nw]  # x interp
+                accumulate = max(1, np.interp(ni, xi, [1, 64 / BATCH_SIZE]).round())
+                for j, x in enumerate(optimizer.param_groups):
+                    # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
+                    x["lr"] = np.interp(ni, xi, [hyp["warmup_bias_lr"] if j == 2 else 0.0, x["initial_lr"] * lf(epoch)])
+                    if "momentum" in x:
+                        x["momentum"] = np.interp(ni, xi, [hyp["warmup_momentum"], hyp["momentum"]])
+
             # Forward
-            with amp.autocast(enabled=cuda):
+            with amp.autocast():
                 pred = model(imgs)  # forward
-                loss, loss_items = compute_loss_ota(pred, targets.to(device), imgs)  # loss scaled by batch_size
+                if "loss_ota" not in hyp or hyp["loss_ota"] == 1:
+                    loss, loss_items = compute_loss_ota(pred, targets.to(device), imgs)  # loss scaled by batch_size
+                else:
+                    loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
 
             # Backward
             scaler.scale(loss).backward()
@@ -177,30 +230,38 @@ def train(hyp, device):
                 if ema:
                     ema.update(model)
 
+            # Print
+            mloss = (mloss * i + loss_items) / (i + 1)  # update mean losses
+            mem = "%.3gG" % (torch.cuda.memory_reserved() / 1e9 if torch.cuda.is_available() else 0)  # (GB)
+            s = ("%10s" * 2 + "%10.4g" * 6) % ("%g/%g" % (epoch, EPOCHS - 1), mem, *mloss, targets.shape[0], imgs.shape[-1])
+            pbar.set_description(s)
+
             # end batch ------------------------------------------------------------------------------------------------
         scheduler.step()
-        # end epoch ----------------------------------------------------------------------------------------------------
-    # end training
-    
-    # Save the last model
-    ckpt = {
-        'epoch': EPOCHS,
-        'model': deepcopy(model).half(),
-        'ema': deepcopy(ema.ema).half(),
-        'updates': ema.updates,
-        'optimizer': optimizer.state_dict()
-    }
-    torch.save(ckpt, 'runs/train/prune/weight.pt')
-    del ckpt
-    
+        
+        # mAP
+        ema.update_attr(model, include=["yaml", "nc", "hyp", "gr", "names", "stride", "class_weights"])
+        final_epoch = epoch + 1 == EPOCHS
+        results, maps, times = test.test(data_dict, batch_size=BATCH_SIZE * 2, imgsz=640, model=ema.ema, 
+                                         dataloader=testloader, save_dir=save_dir, verbose=final_epoch, 
+                                         plots=final_epoch, compute_loss=compute_loss)
+
+        # Write
+        with open(results_file, "a") as f:
+            f.write(s + "%10.4g" * 7 % results + "\n")  # append metrics, val_loss
+
+        # Update best mAP
+        fi = fitness(np.array(results).reshape(1, -1))  # weighted combination of [P, R, mAP@.5, mAP@.5-.95]
+        if fi > best_fitness:
+            best_fitness = fi
+            log = {"epoch": epoch, "best_fitness": best_fitness, "training_results": results_file.read_text(), 
+                   "model": deepcopy(model).half(), "ema": deepcopy(ema.ema).half(), "updates": ema.updates, 
+                   "optimizer": optimizer.state_dict(), "wandb_id": None}
+            torch.save(log, weight_pt)
+            del log
+
     torch.cuda.empty_cache()
+    return results
 
 if __name__ == '__main__':
-    set_logging(-1)
-    device = select_device('0', batch_size=BATCH_SIZE)
-
-    # Hyperparameters
-    with open("data/hyp.scratch.p5.yaml") as f:
-        hyp = yaml.load(f, Loader=yaml.SafeLoader)  # load hyps
-
-    train(hyp, device)
+    train()
